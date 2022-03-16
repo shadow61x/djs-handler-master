624a05c0cffa41f9d1d5b3baec2c071d1d3d9a71
shadow <67285673+shadow61x@users.noreply.github.com>
2022-03-17 02:57:55 +0300

    Add files via upload

--- SlashCommands/info/ping.js
+++ SlashCommands/info/ping.js
@@ -1,1 +1,16 @@
+const { Client, CommandInteraction } = require("discord.js");
 
+module.exports = {
+    name: "ping",
+    description: "Bot geçikmesi",
+    type: 'CHAT_INPUT',
+    /**
+     *
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (client, interaction, args) => {
+        interaction.followUp({ content: `${client.ws.ping}ms!` });
+    },
+};
